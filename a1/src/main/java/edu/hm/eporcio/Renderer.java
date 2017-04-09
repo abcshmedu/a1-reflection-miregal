27a84dfae92c11c726639419d6bfbd96160b3057
Elias Porcio <gggggfff@web.de>
2017-04-09 21:39:19 +0200

    Fixed tests, added array renderer, fixed custom renderer functionality.

--- a1/src/main/java/edu/hm/eporcio/Renderer.java
+++ a1/src/main/java/edu/hm/eporcio/Renderer.java
@@ -25,29 +25,35 @@ public class Renderer {
 	 * @throws IllegalArgumentException
 	 * @throws InvocationTargetException
 	 * @throws ClassNotFoundException
+	 * @throws InstantiationException 
 	 */
-	public String render() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException{
+	public String render() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException, InstantiationException{
 		
 		String result = "";
 		
 		Class<?> toRender = obj.getClass();
-		String objectClass = "Instance of " + obj.getClass().getName().toString() + ":\n";
-		Field[] fields = toRender.getFields();
+		result += "Instance of " + obj.getClass().getName() + ":\n";
+		Field[] fields = toRender.getDeclaredFields();
+		Field.setAccessible(fields, true);
 		for (Field field : fields) {
 			if (field.getAnnotation(edu.hm.eporcio.RenderMe.class) != null) {
+				result += field.getName();
+				result += " (Type ";
 				String renderClassFQDN = (String) field.getAnnotation(edu.hm.eporcio.RenderMe.class).with();	//retrieving annotation argument
-				String value = "";
 				if(renderClassFQDN.length() != 0){	//use specified renderer to get field value, or toString if none was specified
-					value = (String) Class.forName(renderClassFQDN).getMethod("render", field.getType()).invoke(field);
+					Class<?> renderClass = Class.forName(renderClassFQDN);
+					Object renderObject = renderClass.getConstructor().newInstance();
+					result += (String)renderClass.getMethod("render", field.getType()).invoke(renderObject, field.get(obj));
 				}
 				else{
 					Object fieldContent = field.get(obj);
-					value = fieldContent.toString();
+					result += field.getType().getName();
+					result += "): ";
+					result += fieldContent.toString();
 				}
-				
-				result = result + field.getName() + " (Type " + field.getType().getName() + "): " + value + "\n";
+				result += "\n";
 			}
 		}
-		return objectClass + result;
+		return result;
 	}
 }
--- a1/src/test/java/edu/hm/eporcio/RenderTest.java
+++ a1/src/test/java/edu/hm/eporcio/RenderTest.java
@@ -10,44 +10,48 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-//@RunWith(value=Parameterized.class)
+@RunWith(value=Parameterized.class)
 public class RenderTest 
 {
 
 	 private SomeClass toRender;      
 	 private Renderer renderer;
-//	 private String expectedString;
+	 private String expectedString;
 	 
-//	 RenderTest(int value, String msg)
-//	 {
-//		 this.toRender = new SomeClass(value);
-//		 this.expectedString = msg;
-//		 renderer = new Renderer(toRender);
-//	 }
-	 
-	 @Before      
-	 public void setUp() 
-	 {          
-		 toRender = new SomeClass(5);          
-		 renderer = new Renderer(toRender);      
+	 public RenderTest(int value, String msg)
+	 {
+		 this.toRender = new SomeClass(value);
+		 this.expectedString = msg;
+		 renderer = new Renderer(toRender);
 	 }
-//	 @Parameter
-//	 public Collection<Object[]> data()
-//	 {
-//		 return Arrays.asList(new Object[][]
-//		 {
-//			 {5, "I"},
-//			 {2, "P"}
-//			 
-//		 }); 
-//	 }
+	 
+	 @Parameters
+	 public static Collection<Object[]> data()
+	 {
+		 return Arrays.asList(new Object[][]
+		 {
+			 {
+				 5, 
+				 "Instance of edu.hm.eporcio.SomeClass:\n" 
+				 + "foo (Type int): 5\n"
+				 + "array (Type int[]) [1, 2, 3, ]\n"
+				 + "date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n"
+			 },
+			 {
+				 2, 
+				 "Instance of edu.hm.eporcio.SomeClass:\n" 
+				 + "foo (Type int): 2\n"
+				 + "array (Type int[]) [1, 2, 3, ]\n"
+				 + "date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n"
+			 }
+		 }); 
+	 }
 	    
 	 @Test    
 	 public void testRendering() throws Exception 
-	 {      
-		 assertEquals("Instance of edu.hm.eporcio.SomeClass:\n" 
-		 + "foo (Type int): 5\n" //\narray (Type int[]) [1, 2, 3, ]\n"
-		 + "date (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n", renderer.render());      
+	 {    
+		 assertEquals(expectedString, renderer.render());      
 	 }
 }
